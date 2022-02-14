e013b6f37a63148ed3d8c53b55cb03b08a0ddad8
Jae Cho <jjaecho@gmail.com>
2022-02-14 06:37:56 -0500

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Jae Cho on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
