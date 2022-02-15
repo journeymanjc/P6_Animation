d48cffb3fe672da075d3e88bd3b4ee450ad1bf23
Jae Cho <jjaecho@gmail.com>
2022-02-14 22:34:12 -0500

    Day 32 implicit animation

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	@State private var animationAmount = 1.0
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		 
+		 print(animationAmount)
+		 
+//		 Button("Click Me"){
+//
+//		 }
+//		 .padding(50)
+//		 .background(.red)
+//		 .foregroundColor(.white)
+//		 .clipShape(Circle())
+//		 .overlay(
+//				Circle()
+//					.stroke(.red)
+//					.scaleEffect(animationAmount)
+//					.opacity(2 - animationAmount)
+//					.animation(
+//						.easeInOut(duration: 1)
+//							.repeatForever(autoreverses: false),
+//						value: animationAmount
+//					)
+//		 )
+//		 .onAppear {
+//			 animationAmount = 6
+//		 }
+		 
+		 return VStack{
+			 Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+			 Spacer()
+			 Button("Tap Me"){
+				 animationAmount += 1
+			 }
+			 .padding(40)
+			 .background(.red)
+			 .foregroundColor(.white)
+			 .clipShape(Circle())
+			 .scaleEffect(animationAmount)
+		 }
     }
 }
 
