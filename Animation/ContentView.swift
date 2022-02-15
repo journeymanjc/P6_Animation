954bdf8bda6b1e350fcaef51251889005d078c3d
Jae Cho <jjaecho@gmail.com>
2022-02-14 22:46:35 -0500

    Day 32 Explicit modifier

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -9,47 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
 	
-	@State private var animationAmount = 1.0
+	@State private var animationAmount = 0.0
 	
     var body: some View {
-		 
-		 print(animationAmount)
-		 
-//		 Button("Click Me"){
-//
-//		 }
-//		 .padding(50)
-//		 .background(.red)
-//		 .foregroundColor(.white)
-//		 .clipShape(Circle())
-//		 .overlay(
-//				Circle()
-//					.stroke(.red)
-//					.scaleEffect(animationAmount)
-//					.opacity(2 - animationAmount)
-//					.animation(
-//						.easeInOut(duration: 1)
-//							.repeatForever(autoreverses: false),
-//						value: animationAmount
-//					)
-//		 )
-//		 .onAppear {
-//			 animationAmount = 6
-//		 }
-		 
-		 return VStack{
-			 Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
-			 Spacer()
-			 Button("Tap Me"){
-				 animationAmount += 1
+		 Button("Tap Me"){
+			 withAnimation {
+				 animationAmount += 360
 			 }
-			 .padding(40)
-			 .background(.red)
-			 .foregroundColor(.white)
-			 .clipShape(Circle())
-			 .scaleEffect(animationAmount)
+			 
 		 }
-    }
+		 .padding(40)
+		 .background(.red)
+		 .foregroundColor(.white)
+		 .clipShape(Circle())
+		 .rotation3DEffect(.degrees(animationAmount), axis:(x:1, y:1,z:1))
+	 }
 }
 
 struct ContentView_Previews: PreviewProvider {
